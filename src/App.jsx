b79262a8d3e8003e455cccc9b9aef13bbc9b326d
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2023-11-06 01:26:09 +0530

    footer section added with social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Footer from './Components/Shared/Footer/Footer'
 import Navbar from './Components/Shared/Navbar/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className='max-w-7xl mx-auto'>
       <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   )
 }
