96f1b7f190f887b7cf949313f442bf9f70491bf0
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2023-11-07 16:47:24 +0530

    add job page created

--- src/Components/AddJob/AddJob.jsx
+++ src/Components/AddJob/AddJob.jsx
@@ -111,7 +111,7 @@ const AddJob = () => {
             </label>
             <input
               name="jobMinPrice"
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-4 leading-tight focus:outline-none focus:bg-white"
               id="grid-min-price"
               type="text"
               placeholder="Minimum Price"
