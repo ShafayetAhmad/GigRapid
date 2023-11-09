a6b59bbe11055cc1c184fda6feabde29de1979ae
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2023-11-09 17:29:25 +0530

    spinner added

--- src/Components/CategoryTabs/GetTabData/GetTabData.jsx
+++ src/Components/CategoryTabs/GetTabData/GetTabData.jsx
@@ -5,30 +5,45 @@ import SingleCard from "../SingleCard/SingleCard";
 
 const GetTabData = ({ category }) => {
   const [tabData, setTabData] = useState([]);
+  const [loadingData, setLoadingData] = useState(true);
   useEffect(() => {
     const getData = async () =>
       await axios
-        .get(`https://gig-rapid-server.vercel.app/getTabData?Category=${category}`)
+        .get(
+          `https://gig-rapid-server.vercel.app/getTabData?Category=${category}`
+        )
         .then((res) => {
           // console.log(res.data.slice(0, 4));
           // setTabData(res.data.slice(0, 4));
           setTabData(res.data);
+          setLoadingData(false);
         })
         .catch((error) => {
           console.log(error);
+          setLoadingData(false);
         });
     getData();
   }, [category, setTabData]);
   return (
     <div>
       <h2 className="text-5xl my-8 text-center font-bold">
-        Bid on the Best <span className="text-green-700">{tabData[0]?.JobCategory}</span> Job Available
+        Bid on the Best{" "}
+        <span className="text-green-700">{tabData[0]?.JobCategory}</span> Job
+        Available
       </h2>
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
-        {tabData.map((singleData) => (
-          <SingleCard key={singleData._id} job={singleData}></SingleCard>
-        ))}
-      </div>
+      {loadingData ? (
+        <div className="text-center">
+          {" "}
+          <p>Loading Data</p>
+          <span className="loading loading-spinner loading-lg"></span>
+        </div>
+      ) : (
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
+          {tabData.map((singleData) => (
+            <SingleCard key={singleData._id} job={singleData}></SingleCard>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
